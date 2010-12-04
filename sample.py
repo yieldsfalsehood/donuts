756a301db47e01eaf0893c2af6b7a4f132559d19
E. S <yieldsfalsehood@gmail.com>
2010-12-04 01:31:24 -0500

    Add first version of css translating code
    This commit includes the CSS themes as pulled from SHJS, plus the code
    used  to translate those  themes to  a format  suitable for  the latex
    listings package.

---
+++ sample.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import sys
+
+def main(argc, argv):
+    """
+    Here is a docstring.
+    """
+    print 'hello, world'
+    return 0
+
+if __name__ == '__main__':
+    main(len(argv), argv)
+
